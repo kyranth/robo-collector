7a3e3cecf284cff731c235a9c381f8b909cf4174
TechHead <techhead047@gmail.com>
2019-07-12 20:39:37 -0400

    CleanUp

--- main/main.ino
+++ main/main.ino
@@ -11,31 +11,27 @@ unsigned long key_value = 0;
 motor leftMotor(10, 9); // CW, CCW
 motor rightMotor(6, 5);
 
+
+
 void robotForward(int driveSpeed, int driveTime) {
   leftMotor.forward(driveSpeed);
   rightMotor.backward(driveSpeed);
-  delay(driveTime);
-}
+  delay(driveTime); }
 
 void robotBackward(int driveSpeed, int driveTime) {
   leftMotor.backward(driveSpeed);
   rightMotor.forward(driveSpeed);
-  delay(driveTime);
-}
+  delay(driveTime); }
 
 void turnLeft(int turnAngle) {
   leftMotor.forward(240);
   rightMotor.forward(240);
-  delay(turnAngle);
-
-}
+  delay(turnAngle); }
 
 void turnRight(int turnAngle) {
   leftMotor.backward(240);
   rightMotor.backward(240);
-  delay(turnAngle);
-
-}
+  delay(turnAngle); }
 
 void setup () {
   Serial.begin(9600);
