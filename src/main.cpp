e305c13eb44c9671d5ba475a05774a4bb0ef1bbf
K-084 <techhead047@gmail.com>
2019-08-05 13:49:34 -0400

    Added better comment support, and clean up

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-/************************************************************************************************************************************************************************                                              
- * - Author               : Shafiqur Khan, Lena Khoshrof, John (Yanni) Kitsios
- * - Main purpose         : iTEST Activity                                                                               
- * - Copyright (c) holder : All rights reserved
- * - License              : iTEST 
- * - Date                 : 07/26/2019
- * ***********************************************************************************************************************************************************************/
+/***********************************************************************************                                              
+ ** - Author               : Shafiqur Khan, Lena Khoshrof, John (Yanni) Kitsios
+ ** - Main purpose         : iTEST Activity                                                                               
+ ** - Copyright (c) holder : All rights reserved
+ ** - License              : iTEST 
+ ** - Date                 : 07/26/2019
+ * *******************************************************************************/
 
 /*********************************** NOTE **************************************/
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions are met:
 
 // * Redistributions of source code must retain the above copyright notice, this
 //  list of conditions and the following disclaimer.
@@ -22,18 +22,19 @@
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED
 
 /*
-    Scorpion(Clawbot)
-    TEAM: {
-      Dr. Walkot, Mr. Kiefer, Shafiqur Khan, Nischal Thapa
-      Lena Khosrof, John (Yanni) Kitsios, Dushan Persaud
-    }
 
-    Configuration:
-    Left Motor(10, 9)     leftColorSensor(11)       rightColorSensor(13)
-    Right Motor(6, 6)     centerColorSensor(12)     IRSensor()
-    ClawMotor(7, 8)       elbowMotor(2, 4)          RGB Led(11, 12, 13)
+  * Scorpion(Clawbot)
+  *  TEAM: {
+  *    Dr. Walkot, Mr. Kiefer, Shafiqur Khan, Nischal Thapa
+  *    Lena Khosrof, John (Yanni) Kitsios, Dushan Persaud
+  *  }
 
- */
+*    Configuration:
+*    Left Motor(10, 9)     leftColorSensor(11)       rightColorSensor(13)
+*    Right Motor(6, 6)     centerColorSensor(12)     IRSensor()
+*    ClawMotor(7, 8)       elbowMotor(2, 4)          RGB Led(11, 12, 13)
+
+*/
 
 #include "motor.h"
 #include "sensor.h"
